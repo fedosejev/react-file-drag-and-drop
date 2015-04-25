0d2e4b1b23ec9112937fb8f6de2a8974ca1dcbf9
Artemij Fedosejev <artemij.fedosejev@gmail.com>
2015-04-25 21:20:46 +0100

    Add source and description

---
+++ FileDragAndDrop.react.js
@@ -0,0 +1,88 @@
+var React = require('react');
+
+var FileDragAndDrop = React.createClass({
+
+  propTypes: {
+    onDragStart: React.PropTypes.func,
+    onDrop: React.PropTypes.func.isRequired,
+    onDragEnter: React.PropTypes.func,
+    onLeave: React.PropTypes.func,
+    onDragOver: React.PropTypes.func,
+    onDrop: React.PropTypes.func,
+    onDragEnd: React.PropTypes.func
+  },
+
+  handleDragStart: function (event) {
+    if (typeof this.props.onDragStart === 'function') {
+      this.props.onDragStart(event);
+    }
+  },
+
+  handleDrag: function (event) {
+    if (typeof this.props.onDrag === 'function') {
+      this.props.onDrag(event);
+    }
+  },
+
+  handleDragEnter: function (event) {
+    if (typeof this.props.onDragEnter === 'function') {
+      this.props.onDragEnter(event);
+    }
+  },
+
+  handleDragLeave: function (event) {
+    if (typeof this.props.onDragLeave === 'function') {
+      this.props.onDragLeave(event);
+    }
+  },
+
+  handleDragOver: function (event) {
+    event.preventDefault();
+
+    if (typeof this.props.onDragOver === 'function') {
+      this.props.onDragOver(event);
+    }
+  },
+
+  handleDrop: function (event) {
+    event.preventDefault();
+    event.stopPropagation();
+
+    if (event.dataTransfer.files.length > 0) {
+      if (typeof this.props.onDrop === 'function') {
+        this.props.onDrop(event.dataTransfer);
+      }
+    }
+  },
+
+  handleDragEnd: function (event) {
+    if (typeof this.props.onDragEnd === 'function') {
+      this.props.onDragEnd(event);
+    }
+  },
+
+  render: function () {
+    var style = {
+      width: '100%',
+      height: '100%'
+    }
+
+    return (
+      <div
+        onDragStart={this.handleDragStart}
+        onDrag={this.handleDrop}
+        onDragEnter={this.handleDragEnter}
+        onDragLeave={this.handleDragLeave}
+        onDragOver={this.handleDragOver}
+        onDrop={this.handleDrop}
+        onDragEnd={this.handleDragEnd}
+        style={style}>
+
+        {this.props.children}
+
+      </div>
+    );
+  }
+});
+
+module.exports = FileDragAndDrop;
--- README.md
+++ README.md
@@ -1,2 +1,37 @@
-# react-file-drag-and-drop
+# React File Drag and Drop Component
 A simple React component that handles file drag and drop.
+
+## How to use
+
+```javascript
+var FileDragAndDrop = require('react-file-drag-and-drop');
+
+var Example = React.createClass({
+
+  handleDrop: function (dataTransfer) {
+    var files = dataTransfer.files;
+
+    // Do something with dropped files...
+  },
+
+  render: function () {
+    var style = {
+      width: '100px',
+      height: '100px'
+    };
+
+    return (
+      <div>
+        <h1>Please drop your files</h1>
+        <div style={style}>
+          <FileDragAndDrop onDrop={this.handleDrop} filesOnly={true}>
+
+          </FileDragAndDrop>
+        </div>
+      </div>
+    );
+  }
+});
+
+module.exports = Example;
+```
